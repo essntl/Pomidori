66732041d08b2dfa2b770bfe7664311a6dbd9078
essential <artemkotovno@proton.me>
2026-01-23 14:53:55 +0100

    Dark Mode implementation, styling for dark mode, localStorage memory. Minor UI and structure tweaks

---
+++ darkmode.js
@@ -0,0 +1,19 @@
+const darkModeToggle = document.querySelector('.dark-toggle-div input[type="checkbox"]')
+
+if(localStorage.getItem("darkMode") === "true") {
+  document.body.classList.add("dark-mode");
+  darkModeToggle.checked = true;
+}
+
+
+function toggleDarkMode() {
+  if(document.body.classList.contains("dark-mode") === true) {
+    document.body.classList.remove("dark-mode")
+    localStorage.setItem("darkMode", document.body.classList.contains("dark-mode"));
+  } else {
+  document.body.classList.add("dark-mode")
+  localStorage.setItem("darkMode", document.body.classList.contains("dark-mode"));
+  }
+}
+
+darkModeToggle.addEventListener("change", toggleDarkMode);
