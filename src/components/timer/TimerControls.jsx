19d765fcdc38ee4fe481df39e7000f6615619f7c
essential <artemkotovno@proton.me>
2026-02-03 21:53:40 +0100

    fixed some bug

--- src/components/timer/TimerControls.jsx
+++ src/components/timer/TimerControls.jsx
@@ -19,7 +19,7 @@ function TimerControls() {
       <div className="flex gap-2 justify-center items-center">
         <button
           className="rounded-md transition-colors duration-300 bg-pink-200 hover:bg-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700px-4 py-2 dark:text-white min-w-25"
-          onClick={reset}
+          onClick={() => reset()}
         >
           Reset
         </button>
